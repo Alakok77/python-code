226120967011a3b0aabe3bfafaf913bbb811e438
Alakok77 <top.p1234558@gmail.com>
2024-09-19 21:38:26 +0700

    BusStop

--- week10/BusStop I.py
+++ week10/BusStop I.py
@@ -5,14 +5,18 @@ def main():
     station = int(input())
     wait = []
     buses = []
+    info = []
     complete = 0
     for _ in range(station):
         wait = input().split()
-        while buses.count(wait[0]) > 0:
-            buses.remove(wait[0])
+        info.append(wait)
+    info.sort(key=lambda x: x[0])
+    for human in info:
+        while buses.count(human[0]) > 0:
+            buses.remove(human[0])
             complete += 1
-        for i in wait[1:]:
-            if int(i) > int(wait[0]) and len(buses) < most:
+        for i in human[1:]:
+            if int(i) > int(human[0]) and len(buses) < most:
                 buses.append(i)
             if len(buses) == most:
                 break
