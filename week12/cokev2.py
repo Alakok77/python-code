66618693e17b6a0c4b464d3fa0d11f9c1d86f390
Alakok77 <top.p1234558@gmail.com>
2024-09-23 09:27:04 +0700

    again

--- week12/cokev2.py
+++ week12/cokev2.py
@@ -4,7 +4,7 @@ def coke(price, cap, discount, wanted, my_cap):
     if wanted < 1:
         return 0
     if my_cap >= cap:
-        return discount + coke(price, cap, discount, wanted - 1, my_cap - cap)
+        return discount + coke(price, cap, discount, wanted - 1, my_cap - cap + 1)
     return price + coke(price, cap, discount, wanted - 1, my_cap + 1)
 
 print(coke(int(input()), int(input()), int(input()), int(input()), 0))
